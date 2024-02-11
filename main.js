79b36b3b3aaceade4f35a1af041b1115492b9718
jenniferericson <jenniferericson813@gmail.com>
2024-02-11 13:38:21 +0100

    fixed database connection for get all, and create

--- main.js
+++ main.js
@@ -3,81 +3,26 @@ const cors = require("cors");
 const app = express();
 const port = 3000; //radiofrekvens
 
-const { sequelize, Employee } = require('./models');
+const { sequelize, Product } = require('./models');
 const migrationhelper = require('./migrationhelper');
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors());
 
-const products = [{
-    name: "Hyal Reyouth Toner",
-    brand: "Dr. Ceuracle",
-    price: 299 + "kr",
-    rating: 5,
-    id: 1
-},
-{
-    name: "Rich Moist Soothing Cream",
-    brand: "Klairs",
-    price: 399 + "kr",
-    rating: 5,
-    id: 2
-},
-{
-    name: "Natural Repair Serum",
-    brand: "Hyggee",
-    price: 559 + "kr",
-    rating: 5,
-    id: 3
-},
-{
-    name: "Rice Toner",
-    brand: "I'm From",
-    price: 449 + "kr",
-    rating: 4.5,
-    id: 4
-},
-{
-    name: "Vegan Kombucha Tea Essence",
-    brand: "Dr. Ceuracle",
-    price: 499 + "kr",
-    rating: 5,
-    id: 5
-},
-{
-    name: "Supple Preparation Unscented Toner",
-    brand: "Klairs",
-    price: 299 + "kr",
-    rating: 5,
-    id: 6
-},
-{
-    name: "Royal Vita Propolis 33 Ampoule",
-    brand: "Dr. Ceuracle",
-    price: 349 + "kr",
-    rating: 4.5,
-    id: 7
-},
-{
-    name: "Advanced Snail 96 Mucin Power Essence",
-    brand: "COSRX",
-    price: 299 + "kr",
-    rating: 4.5,
-    id: 8
-},
-{
-    name: "All In One Cream",
-    brand: "Hyggee",
-    price: 499 + "kr",
-    rating: 4.5,
-    id: 9
-},
-];
 
-app.get("/api/products",(req,res) => {
+
+app.get("/api/products",async (req,res) => {
     //hämta alla produkter
-    res.json(products)
+    let products = await Product.findAll()
+    let result = products.map(p=>({
+        productId: p.id,
+        name: p.name,
+        brand: p.brand,
+        price: p.price,
+        rating: p.rating
+    }))
+     res.json(result)
 });
 
 app.get("/api/products/:productId",(req,res) => {
@@ -89,21 +34,16 @@ app.get("/api/products/:productId",(req,res) => {
     res.json(product)
 });
 
-function getNextId(){
-    let m = Math.max(...products.map(product => product.id))
-    return m + 1
-}
 
-app.post("/api/products",(req,res) => {
+app.post("/api/products", async (req,res) => {
     //skapa nytt objekt
-    const product = {
+
+    await Product.create({
         name : req.body.name,
         brand: req.body.brand,
         price: req.body.price,
-        rating: req.body.rating,
-        id:getNextId()
-    }
-    products.push(product)
+        rating: req.body.rating
+    })
 
     res.status(201).send("Created");
 });
@@ -137,5 +77,5 @@ app.put('/api/products/:productId',(req,res)=>{
 app.listen(port, async () => {
     await migrationhelper.migrate()
     await sequelize.authenticate()
-    console.log(`Example app listening2 on port ${port}`)
-});
+    console.log(`Example app listening on port ${port}`)
+}); 
